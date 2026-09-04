76261375b4836339d27fbd11d269f35c66cc9d56
Tyler <25913563+itsBOTzilla@users.noreply.github.com>
2026-09-04 15:11:20 -0500

    fix(questhelper): allow local Misthalin item pickups (#25)

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/questhelper/QuestHelperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/questhelper/QuestHelperPlugin.java
@@ -79,6 +79,7 @@ import java.util.stream.Collectors;
 
 @PluginDescriptor(
 	name = "Quest Helper",
+	version = "1.0.1",
 	description = "Helps you with questing",
 	tags = { "quest", "helper", "overlay" }
 )
